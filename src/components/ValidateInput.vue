95fc1a446807f5497454ac27c16f173f82a0c3b1
liqiang <563165732@qq.com>
2022-04-15 14:38:49 +0800

    输入框

---
+++ src/components/ValidateInput.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="validate-input-container pb-3">
+    <input type="text" :value="inputRef.val" @input="updateValue" @blur="validateInput" class="form-control" :class="{'isInvalid': inputRef.error}">
+    <span v-if="inputRef.error" class="invalid-feedback">
+      {{inputRef.message}}
+    </span>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive, PropType } from 'vue'
+
+const emailReg = /^[a-zA-Z0-9]/;
+interface RuleProp {
+  type: 'required' | 'email';
+  message: string;
+}
+export type RulesProp = RuleProp[]; 
+export default defineComponent({
+  props: {
+    rules: Array as PropType<RulesProp>,
+    modelValue: String
+  },
+  name: 'ValidateInput',
+  setup(props, context) {
+    const inputRef = reactive({
+      val: props.modelValue || '',
+      error: false,
+      message: ''
+    })
+    const updateValue = (e: KeyboardEvent) => {
+      const targetValue = (e.target as HTMLInputElement).value;
+      inputRef.val = targetValue;
+      context.emit('update:modelValue', targetValue)
+    }
+    const validateInput = () => {
+      if (props.rules) {
+        const allPassed = props.rules.every(rule => {
+          let passed = true;
+          inputRef.message = rule.message;
+
+          switch(rule.type) {
+            case 'required':
+              passed = (inputRef.val.trim() !== ''); break;
+            case 'email':
+              passed = emailReg.test(inputRef.val);  break;
+            default:
+              break;
+          }
+          return passed;
+        })
+        inputRef.error = !allPassed
+      }
+    }
+    console.log(inputRef)
+    return { inputRef, validateInput, updateValue }
+  }
+})
+</script>
+
+<style lang="less" scoped>
+.invalid-feedback {
+  display: block !important;
+}
+</style>
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,32 +1,41 @@
 <template>
   <div class="container">
     <GlobalHeader :user="currentUser" />
+    <div class="mb-3">
+      <ValidateInputVue v-model="emailVal" :rules="emailRules" />
+      {{ emailVal }}
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 // defineComponet是一个函数，没什么作用，专门为了支持ts出现的。
-import { defineComponent, } from 'vue';
+import { defineComponent, reactive, ref } from 'vue';
 
 import GlobalHeader, { UserProps } from '@/components/GlobalHeader.vue';
-
+import ValidateInputVue, { RulesProp } from '@/components/ValidateInput.vue';
 const currentUser: UserProps = {
   isLogin: true,
   name: 'Lee'
 }
-
+let emailReg = /^[a-zA-Z0-9]/;
 export default defineComponent({
   name: 'Main',
   components: {
     GlobalHeader,
+    ValidateInputVue,
   },
   props: {
     
   },
   setup(props, context) {
-    
+    const emailRules: RulesProp = [
+      { type: 'required', message: '不能为空' },
+      { type: 'email', message: '格式不正确' },
+    ]
+    let emailVal = ref('1234');
 
-    return { currentUser }
+    return { currentUser, emailRules, emailVal }
   }
 });
 </script>
